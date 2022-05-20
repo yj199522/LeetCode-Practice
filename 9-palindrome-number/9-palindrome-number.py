f7b9230dbdce4469108763babf23f9fe1bd1a0d3
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-05-20 17:06:43 -0400

    Time: 77 ms (63.28%), Space: 13.9 MB (60.33%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -2,8 +2,6 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x is None or x < 0:
             return False
-        
         data = str(x)
-        
         return data == data[::-1]
         
