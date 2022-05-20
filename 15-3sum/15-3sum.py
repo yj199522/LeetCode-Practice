72092375bb0dcc31e66e1b216b6b05a1f92bc1ee
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-05-20 18:32:44 -0400

    Time: 4919 ms (14.95%), Space: 18.2 MB (22.78%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -6,6 +6,8 @@ class Solution:
         nums = sorted(nums)
         result = []
         for i in range(0, len(nums) - 2):
+            if nums[i] > 0:
+                break
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             left = i+1
