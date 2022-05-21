6f35657ead36287faf63673c80d3bc5744bafb98
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-05-20 22:59:34 -0400

    Time: 378 ms (5.29%), Space: 14.1 MB (32.73%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -7,19 +7,45 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         if list1 is None and list2 is None:
             return None
-        data = []
-        while list1 is not None:
-            data.append(list1.val)
-            list1 = list1.next
-        while list2 is not None:
-            data.append(list2.val)
-            list2 = list2.next
-        data = sorted(data)
-        result = ListNode(data[0])
-        dummy = result
-        for i in range(1,len(data)):
-            node = ListNode(data[i])
-            dummy.next = node
-            dummy = dummy.next
-        return result
+        
+        if list1 is None:
+            return list2
+        
+        if list2 is None:
+            return list1
+    
+        result = ListNode()
+        prevList = result
+        
+        while list1 and list2:
+            print(list1)
+            if list1.val <= list2.val:
+                prevList.next = list1
+                list1 = list1.next
+            else:
+                prevList.next = list2
+                list2 = list2.next
+            prevList = prevList.next
+        if list1:
+            prevList.next = list1
+        
+        if list2:
+            prevList.next = list2
+        
+        return result.next
+#         data = []
+#         while list1 is not None:
+#             data.append(list1.val)
+#             list1 = list1.next
+#         while list2 is not None:
+#             data.append(list2.val)
+#             list2 = list2.next
+#         data = sorted(data)
+#         result = ListNode(data[0])
+#         dummy = result
+#         for i in range(1,len(data)):
+#             node = ListNode(data[i])
+#             dummy.next = node
+#             dummy = dummy.next
+#         return result
             
