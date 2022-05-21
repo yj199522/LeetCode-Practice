bf97855d1f1f318e4ecaeb1dba74d2c090a85bf3
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-05-20 22:20:29 -0400

    Time: 28 ms (95.26%), Space: 13.9 MB (24.44%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -2,23 +2,32 @@ class Solution:
     def isValid(self, s: str) -> bool:
         openBrak = ['(', '{', '[']
         closeBrak = [')', '}', ']']
+        bracket = {
+            '(': ')',
+            '{': '}',
+            '[': ']'
+        }
         
-        if s is None or s[0] in closeBrak:
+        if s is None or s[0] not in bracket:
             return False
         
         i = 0
         check = []
         while i < len(s):
-            if s[i] in openBrak:
-                check.append(s[i])
-            elif s[i] in closeBrak:
-                if len(check) == 0:
+            if s[i] in bracket:
+                check.append(bracket[s[i]])
+            # elif s[i] in closeBrak:
+            #     if len(check) == 0:
+            #         return False
+            #     checkBrackIndex = openBrak.index(check[-1])
+            #     closeBrackIndex = closeBrak.index(s[i])
+            #     if closeBrackIndex != checkBrackIndex:
+            #         return False
+            #     check.pop()
+            else:
+                if len(check)==0 or s[i]!=check[-1]:
                     return False
-                checkBrackIndex = openBrak.index(check[-1])
-                closeBrackIndex = closeBrak.index(s[i])
-                if closeBrackIndex != checkBrackIndex:
-                    return False
-                check.pop()
+                check.pop();
             i+=1
         if len(check) == 0:
             return True
