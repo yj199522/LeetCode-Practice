c2c7080f48d596648c08a11656d312024927f924
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-05-21 17:36:40 -0400

    Time: 64 ms (46.74%), Space: 13.9 MB (86.55%) - LeetHub

--- 38-count-and-say/38-count-and-say.py
+++ 38-count-and-say/38-count-and-say.py
@@ -3,14 +3,30 @@ class Solution:
         if n == 1:
             return '1'
         
-        prev = self.countAndSay(n-1)
-        res = ''
-        count = 1
+        result = '1'
         
-        for i in range(len(prev)):
-            if i == len(prev) - 1 or prev[i] != prev[i+1]:
-                res+= str(count) + prev[i]
-                count = 1
-            else:
-                count+=1
-        return res
+        for i in range(1, n):
+            count = 1
+            data = '' 
+            for j in range(len(result)):
+                if j == len(result) - 1 or result[j] != result[j+1]:
+                    data+= str(count)+result[j]
+                    count = 1
+                else:
+                    count+=1
+            result = data
+        return result
+#         if n == 1:
+#             return '1'
+        
+#         prev = self.countAndSay(n-1)
+#         res = ''
+#         count = 1
+        
+#         for i in range(len(prev)):
+#             if i == len(prev) - 1 or prev[i] != prev[i+1]:
+#                 res+= str(count) + prev[i]
+#                 count = 1
+#             else:
+#                 count+=1
+#         return res
