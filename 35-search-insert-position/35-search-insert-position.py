253b37911cd4920265499c9835fd3a61befe9197
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-05-22 07:08:13 -0400

    Time: 63 ms (56.74%), Space: 14.7 MB (41.08%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,5 +1,7 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        if nums is None:
+            return 0
         for index, item in enumerate(nums):
             if item >= target: return index
         return len(nums)
