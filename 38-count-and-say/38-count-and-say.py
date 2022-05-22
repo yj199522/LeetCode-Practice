3dcac1a064e71e5722a235b9b65596cab6ef4f1c
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-05-22 07:12:25 -0400

    Time: 73 ms (31.90%), Space: 14 MB (30.00%) - LeetHub

--- 38-count-and-say/38-count-and-say.py
+++ 38-count-and-say/38-count-and-say.py
@@ -13,17 +13,3 @@ class Solution:
             else:
                 count+=1
         return result
-#         if n == 1:
-#             return '1'
-        
-#         prev = self.countAndSay(n-1)
-#         res = ''
-#         count = 1
-        
-#         for i in range(len(prev)):
-#             if i == len(prev) - 1 or prev[i] != prev[i+1]:
-#                 res+= str(count) + prev[i]
-#                 count = 1
-#             else:
-#                 count+=1
-#         return res
