9add942c28f15a2b5eccf6b6a1144d6affc73dac
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-05-23 17:09:52 -0400

    Time: 133 ms (26.31%), Space: 17.3 MB (54.72%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.py
@@ -0,0 +1,18 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        if newInterval is None:
+            return intervals
+        if intervals is None:
+            return [newInterval]
+        result = []
+        
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                result.append(newInterval)
+                return result + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                result.append(intervals[i])
+            else:
+                newInterval = [min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]
+        result.append(newInterval)
+        return result
