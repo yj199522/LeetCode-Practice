9a920431ccfe2ef1259b5a9d558925dfee4c07cf
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-05-23 17:17:52 -0400

    Time: 63 ms (7.28%), Space: 14 MB (5.51%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.py
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -2,11 +2,8 @@ class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         if s is None or len(s) == 0:
             return 0
-        
         s = s.split(' ')
-        
         n = len(s) - 1
-        
         while n!=0:
             if s[n] == '':
                 s.pop()
