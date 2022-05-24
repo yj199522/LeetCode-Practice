380d0ea9c9125dc906470e7b6f92eabe385492fa
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-05-23 20:09:30 -0400

    Time: 139 ms (83.71%), Space: 15.1 MB (59.40%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,5 +1,7 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return 0
         result = 0
         left, right = 0 , 0
         while right < len(nums) - 1:
