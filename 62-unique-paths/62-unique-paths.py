f59a779b1392fbe4115bf3d4530d33cab908bca8
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-05-26 15:22:35 -0400

    Time: 45 ms (46.23%), Space: 14 MB (32.33%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,16 +1,25 @@
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:  
-        memo = {(0,1):1 , (1,0):1, (1,1):1}
+    def uniquePaths(self, m: int, n: int) -> int: 
+        dp=[[0] * (n+1) for _ in range(m+1)]        
+        dp[0][1]=1
+                        
         for row in range(1, m+1):
             for col in range(1, n+1):
-                if row == 1:
-                    memo[(row,col)] = memo[(row, col-1)]
-                elif col == 1:
-                    memo[(row, col)] = memo[(row-1, col)]
-                else:
-                    memo[(row, col)] = memo[(row-1,col)] + memo[(row, col-1)]
+                dp[row][col] = dp[row-1][col] + dp[row][col-1]
+         
+        return dp[-1][-1]
+#         memo = {(0,1):1 , (1,0):1, (1,1):1}
+#         for row in range(1, m+1):
+#             for col in range(1, n+1):
+#                 if row == 1:
+#                     memo[(row,col)] = memo[(row, col-1)]
+#                 elif col == 1:
+#                     memo[(row, col)] = memo[(row-1, col)]
+#                 else:
+#                     memo[(row, col)] = memo[(row-1,col)] + memo[(row, col-1)]
         
-        return memo[(row, col)]
+#         return memo[(row, col)]
+    
                 
                 
         
