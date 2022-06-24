e1d8a2472cf8dc1cf1dc0c614552029adaac08af
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-06-24 14:13:31 -0400

    Time: 1222 ms (74.29%), Space: 25 MB (85.18%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,24 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        return self.splitAndFind(prices,0,len(prices) - 1)
-    
-    def splitAndFind(self, prices, left, right):
-        if left>= right:
-            return 0
-        profit = 0
-        mid = (left + right) // 2
-        leftProfit = self.splitAndFind(prices, left, mid)
-        rightProfit = self.splitAndFind(prices, mid+1, right)
-        calProfit = self.compareAndFind(prices, left, mid, right)
-        profit = max(profit, max(calProfit, max(leftProfit,rightProfit)));
-        return profit
-    
-    def compareAndFind(self, prices, left, mid, right):
-        minStock = prices[left]
-        for i in range(left+1, mid+1):
-            minStock = min(minStock, prices[i])
-        
-        maxStock = 0
-        for i in range(mid+1, right+1):
-            maxStock = max(maxStock, prices[i])
-        return maxStock - minStock
+        max_profit=0
+        buy=prices[0]
+        for i in range(1,len(prices)):
+            profit=prices[i]-buy
+            max_profit=max(max_profit,profit)
+            buy=min(buy,prices[i])
+        return max_profit
